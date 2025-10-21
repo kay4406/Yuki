100471dad1679d688da6c35361051b5b39c95624
kay4406 <nonosujono739@gmail.com>
2025-10-21 13:35:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const TombolSapa = document.getElementByld('sapaButton');
-TombolSapa.addEventListener('click', function() {
-  alert('halo! Terima kasih sudah berkunjung');
+const tombolSapa=document.getElementById('sapaButton');
+tombolSapa.addEventListener('click',function() {
+  alert('Welcome To Store!,Terima Kasih Sudah Berkunjung!');
 });
